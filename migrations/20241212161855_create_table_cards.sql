66c896645590a982161240dd336ca05f8862d996
Vladimir Suragin <v.suragin@yclients.tech>
2024-12-12 19:20:52 +0300

    feat: modules & cards tables

---
+++ migrations/20241212161855_create_table_cards.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE cards (
+  uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  term TEXT NOT NULL,
+  meaning TEXT NOT NULL,
+  module_uuid UUID NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_module FOREIGN KEY(module_uuid) REFERENCES modules(uuid)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE cards;
+-- +goose StatementEnd
